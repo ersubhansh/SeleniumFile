ad2beb111ec0a769741966ab81e2f1c15a25f031
Subhansh <subhansh.sharma@netcracker.com>
2020-04-26 16:49:49 +0530

    GmailPage

--- src/FirstPackage/FBPage.java
+++ src/FirstPackage/FBPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class FirstScript {
+class FirstScript {
     public static void main(String[] args) throws InterruptedException{
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssha7338\\Documents\\netcracker-atp-executor-build-1.4.25\\resources\\components\\webdriver\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
