ada389d9a8c769c1b361fc1c61b9c471391be07f
Subhansh <subhansh.sharma@netcracker.com>
2020-04-26 17:53:08 +0530

    GmailPage

---
+++ src/FirstPackage/GmailTesting.java
@@ -0,0 +1,22 @@
+package FirstPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GmailTesting {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssha7338\\Documents\\netcracker-atp-executor-build-1.4.25\\resources\\components\\webdriver\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("http://gmail.com");
+        driver.findElement(By.id("identifierId")).sendKeys("subhanshcoding@gmail.com");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys("Vidushi1!");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+    }
+}
