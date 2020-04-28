929cdc29487952ca040264d0a1c51785d39c358e
Subhansh <subhansh.sharma@netcracker.com>
2020-04-28 22:56:35 +0530

    AmazonPage

---
+++ src/FirstPackage/AmazonPage.java
@@ -0,0 +1,18 @@
+package FirstPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonPage {
+    public static void main (String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssha7338\\Documents\\netcracker-atp-executor-build-1.4.25\\resources\\components\\webdriver\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("http://amazon.in");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Poco F1");
+        Thread.sleep(3000);
+        driver.findElement(By.className("nav-input")).click();
+    }
+}
